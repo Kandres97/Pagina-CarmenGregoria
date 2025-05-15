a6636db0901918f46838cf9a67f0f50c39828a24
Kandres97 <kandresyiro97@gmail.com>
2025-05-15 17:45:02 -0500

    gregoria

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+    return (
+      <div className="bg-gray-800 text-white min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-500">
+          ¡Tailwind CSS está funcionando!
+        </h1>
+      </div>
+    );
+  }
+  
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HeroSection from './components/home'
-import ServiceSection from './components/servicios' 
-import TestimonialsSection from './components/testimonios'
-import Footer from './components/Footer'
-import ExperienceSection from './components/More'
+import App from './components/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HeroSection />
-    <ServiceSection /> 
-    <ExperienceSection />
-    <TestimonialsSection /> 
-    
-    <Footer />
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
